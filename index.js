3c9f8101a6782b33a463b328e4aab98797bfc0d9
Francois2344 <deloufr@gmail.com>
2021-03-23 16:59:17 +0100

    link DB with mongoose and mongo

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port port!`))
+const mongoose = require("mongoose");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://FDAdmin:Frde2344058616@planningproject.lwyie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+app.listen(port, () => console.log(`Example app listening on port port!`));
