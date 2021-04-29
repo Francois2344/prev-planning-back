d7dedee7f3fdef0daac4128ba4ec01114675441c
Francois2344 <deloufr@gmail.com>
2021-04-29 16:52:42 +0200

    config and write swagger doc for api

--- routes/adminAuth.js
+++ routes/adminAuth.js
@@ -5,6 +5,58 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const AdminAuth = require('../models/adminAuth.model');
 
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *      adminAuth:
+ *          type: object
+ *          required:
+ *              -email
+ *              -passwordHash
+ *          properties:
+ *             email:
+ *              type: string
+ *             passwordHash:
+ *              type: string
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Register
+ */
+
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *      summary: Enregistrer un nouvel administrateur
+ *      parameters:
+ *      - in: path
+ *        name: email, password, passwordVerify
+ *        schema:
+ *          type: string
+ *        required: true
+ *
+ *      tags: [Register]
+ *      responses:
+ *        200:
+ *          description: Admin enregistré
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  items:
+ *                  $ref: '#/components/schemas/adminAuth'
+ *        400:
+ *          description: Erreur de champs, 6 caractères min, compte déja existant, mot de passe non similaire
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  items:
+ *                  $ref: '#/components/schemas/adminAuth'
+ */
+
 // register
 
 router.post('/', async (req, res) => {
@@ -68,6 +120,36 @@ router.post('/', async (req, res) => {
 
 // log in
 
+/**
+ * @swagger
+ * /register/login:
+ *   post:
+ *     summary: Connexion administrateur
+ *     tags: [Login]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/adminAuth'
+ *     responses:
+ *       400:
+ *         description: champs requis
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/adminAuth'
+ *       401:
+ *          description: champs invalide
+ *       500:
+ *         description: server error
+ *         headers:
+ *            Set-Cookie:
+ *              schema:
+ *                type: boolean
+ *                example: token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly
+ */
+
 router.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -106,6 +188,19 @@ router.post('/login', async (req, res) => {
     res.status(500).send();
   }
 });
+/**
+ * @swagger
+ * /register/logout:
+ *   get:
+ *     tags: [Logout]
+ *     security:
+ *      - cookieAuth: []
+ *     description: deconnexion admin
+ *     responses:
+ *       '200':
+ *         description: OK
+ *
+ */
 
 router.get('/logout', (req, res) => {
   res
@@ -115,7 +210,19 @@ router.get('/logout', (req, res) => {
     })
     .send();
 });
-
+/**
+ * @swagger
+ * /register/loggedIn:
+ *   get:
+ *     tags: [LoggedIn]
+ *     description: verification connexion
+ *     responses:
+ *       '200':
+ *          description: OK
+ *          headers:
+ *           schema:
+ *            type: boolean
+ */
 router.get('/loggedIn', (req, res) => {
   try {
     const { token } = req.cookies;
--- routes/site.js
+++ routes/site.js
@@ -1,35 +1,168 @@
 const router = require('express').Router();
 const Site = require('../models/site.model');
 
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *      site:
+ *          type: object
+ *          required:
+ *              -siteName
+ *          properties:
+ *             siteName:
+ *              type: string
+ */
+
+/**
+ * @swagger
+ * /sites:
+ *   get:
+ *      summary: Liste des actions terrains
+ *      parameters:
+ *      - in: path
+ *        name: siteName
+ *        schema:
+ *          type: string
+ *        required: true
+ *
+ *      tags: [Site]
+ *      responses:
+ *        200:
+ *          description: Liste OK
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  items:
+ *                  $ref: '#/components/schemas/site'
+ *        400:
+ *          description: Erreur dans la requête
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  items:
+ *                  $ref: '#/components/schemas/site'
+ */
+
 // ROUTE GET
-router.route('/').get((req, res) => {
-  Site.find()
+router.get('/', async (req, res) => {
+  await Site.find()
     .then((site) => res.json(site))
     .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
+/**
+ * @swagger
+ * /sites/add:
+ *   post:
+ *      summary: Ajouter une action terrain
+ *      parameters:
+ *      - in: path
+ *        name: siteName
+ *        schema:
+ *          type: string
+ *        required: true
+ *
+ *      tags: [Site]
+ *      responses:
+ *        200:
+ *          description: Ajout d'une action terrain OK
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  items:
+ *                  $ref: '#/components/schemas/site'
+ *        400:
+ *          description: Erreur dans la requête
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  items:
+ *                  $ref: '#/components/schemas/site'
+ */
+
 // ROUTE POST
-router.route('/add').post((req, res) => {
+router.post('/add', async (req, res) => {
   const { siteName } = req.body;
 
   const newSite = new Site({ siteName });
 
-  newSite
+  await newSite
     .save()
     .then(() => res.json('Site added'))
     .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
+/**
+ * @swagger
+ * /sites/{id}:
+ *   delete:
+ *      summary: Supprimer une action terrain
+ *      parameters:
+ *      - in: path
+ *        name: siteName
+ *        schema:
+ *          type: string
+ *        required: true
+ *
+ *      tags: [Site]
+ *      responses:
+ *        200:
+ *          description: Action terrain supprimé
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  items:
+ *                  $ref: '#/components/schemas/site'
+ *        400:
+ *          description: Erreur dans la requête
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  items:
+ *                  $ref: '#/components/schemas/site'
+ */
+
 // ROUTE DELETE
-router.route('/:id').delete((req, res) => {
-  Site.findByIdAndDelete(req.params.id)
+router.delete('/:id', async (req, res) => {
+  await Site.findByIdAndDelete(req.params.id)
     .then(() => res.json('Site deleted'))
     .catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
+/**
+ * @swagger
+ * /sites/{id}:
+ *   put:
+ *      summary: Mise à jour d'une action terrain
+ *      parameters:
+ *      - in: path
+ *        name: siteName
+ *        schema:
+ *          type: string
+ *        required: true
+ *
+ *      tags: [Site]
+ *      responses:
+ *        200:
+ *          description: Liste OK
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  items:
+ *                  $ref: '#/components/schemas/site'
+ *        400:
+ *          description: Erreur dans la requête
+ *          content:
+ *            application/json:
+ *              schema:
+ *                  items:
+ *                  $ref: '#/components/schemas/site'
+ */
+
 // ROUTE PUT
-router.route('/:id').put((req, res) => {
-  Site.findByIdAndUpdate(req.params.id)
+router.put('/:id', async (req, res) => {
+  await Site.findByIdAndUpdate(req.params.id)
     .then(() => res.json('Site updated'))
     .catch((err) => res.status(400).json(`Error: ${err}`));
 });
